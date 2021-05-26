fdda43538da91373970d4d8bc9f0d7a9b061dd65
Vinay-Kothiyal <vinaykothiyal9993@gmail.com>
2021-05-26 06:39:59 +0530

    this is commit from user1

--- src/interfaceConcept/Rectangle.java
+++ src/interfaceConcept/Rectangle.java
@@ -4,6 +4,8 @@ public class Rectangle implements Drawable {
 	static int t = 3;
 	public void draw() {
 		System.out.println("Rectangle");
+		System.out.println("add and update1");
+		System.out.println("verify updated");
 
 	}
 
